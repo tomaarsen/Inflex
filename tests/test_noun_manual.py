7949f2fba89f4e3b6707e5568798920c6a92ba7b
Tom Aarsen <Cubiegamedev@gmail.com>
2021-06-06 19:03:52 +0200

    Added some is_singular and is_plural tests with complex patterns
    Also reformatted

--- tests/test_noun_manual.py
+++ tests/test_noun_manual.py
@@ -9,7 +9,6 @@ from inflex import Noun
 
 
 class TestNouns(unittest.TestCase):
-
     def test_is_noun(self):
         noun = Noun("book")
         self.assertFalse(noun.is_verb())
@@ -19,35 +18,42 @@ class TestNouns(unittest.TestCase):
     def test_as_regex(self):
         noun = Noun("brother")
         pattern = noun.as_regex()
-        self.assertEqual(pattern, re.compile("brothers|brother|brethren", re.IGNORECASE),
-                         "Check whether as_regex produces a compiled regex object correctly.")
+        self.assertEqual(
+            pattern,
+            re.compile("brothers|brother|brethren", re.IGNORECASE),
+            "Check whether as_regex produces a compiled regex object correctly.",
+        )
 
     def test_classical_as_regex(self):
         noun = Noun("brother")
         classical = noun.classical()
         pattern = classical.as_regex()
-        self.assertEqual(pattern, re.compile("brother|brethren", re.IGNORECASE),
-                         "Check whether as_regex on classical() produces a compiled regex object correctly.")
+        self.assertEqual(
+            pattern,
+            re.compile("brother|brethren", re.IGNORECASE),
+            "Check whether as_regex on classical() produces a compiled regex object correctly.",
+        )
 
     def test_classical_cache(self):
         noun = Noun("brother")
         classical_one = noun.classical()
         classical_two = noun.classical()
-        self.assertEqual(classical_one, classical_two,
-                         "Cache of Noun(...).classical()")
+        self.assertEqual(classical_one, classical_two, "Cache of Noun(...).classical()")
 
     def test_classical_classical(self):
         classical_one = Noun("brother").classical()
         classical_two = classical_one.classical()
-        self.assertEqual(classical_one, classical_two,
-                         "Noun(...).classical() == Noun(...).classical().classical()")
+        self.assertEqual(
+            classical_one,
+            classical_two,
+            "Noun(...).classical() == Noun(...).classical().classical()",
+        )
 
     def test_classical_modern(self):
         noun = Noun("brother")
         classical = noun.classical()
         modern = classical.modern()
-        self.assertEqual(noun, modern,
-                         "Noun(...) = Noun(...).classical().modern()")
+        self.assertEqual(noun, modern, "Noun(...) = Noun(...).classical().modern()")
 
     def test_repr(self):
         noun = Noun("brother")
@@ -60,43 +66,43 @@ class TestNouns(unittest.TestCase):
 
     def test_indefinite_plural(self):
         test_data = [
-            ('universe', 'universes'),
-            ('uniplex', 'uniplexes'),
-            ('height', 'heights'),
-            ('FSM', 'FSMs'),
-            ('use', 'uses'),
-            ('lady in waiting', 'ladies in waiting'),
-            ('octavo', 'octavos'),
-            ('D', 'Ds'),
-            ('Q', 'Qs'),
-            ('PET', 'PETs'),
-            ('P', 'Ps'),
-            ('erratum', 'errata'),
-            ('once-and-future-king', 'once-and-future-kings'),
-            ('Tth', 'Tths'),
-            ('urn', 'urns'),
-            ('DNR', 'DNRs'),
-            ('N', 'Ns'),
-            ('FACT', 'FACTs'),
-            ('UNESCO representative', 'UNESCO representatives'),
-            ('Oth', 'Oths')]
+            ("universe", "universes"),
+            ("uniplex", "uniplexes"),
+            ("height", "heights"),
+            ("FSM", "FSMs"),
+            ("use", "uses"),
+            ("lady in waiting", "ladies in waiting"),
+            ("octavo", "octavos"),
+            ("D", "Ds"),
+            ("Q", "Qs"),
+            ("PET", "PETs"),
+            ("P", "Ps"),
+            ("erratum", "errata"),
+            ("once-and-future-king", "once-and-future-kings"),
+            ("Tth", "Tths"),
+            ("urn", "urns"),
+            ("DNR", "DNRs"),
+            ("N", "Ns"),
+            ("FACT", "FACTs"),
+            ("UNESCO representative", "UNESCO representatives"),
+            ("Oth", "Oths"),
+        ]
         for sing, plur in test_data:
             with self.subTest():
-                digit = random.randrange(2, 10) # nosec
+                digit = random.randrange(2, 10)  # nosec
                 out = Noun(sing).indefinite(count=digit)
-                self.assertEqual(out, f'{digit} {plur}',
-                                 f"Noun({sing!r}).indefinite(count={digit})")
+                self.assertEqual(
+                    out, f"{digit} {plur}", f"Noun({sing!r}).indefinite(count={digit})"
+                )
 
     def test_possessive(self):
         test_data = [
             ("Tom's", "Toms'"),
             ("book's", "books'"),
             ("brother's", "brothers'"),
-
             ("about Tom's", "about Toms'"),
             ("beneath book's", "beneath books'"),
             ("at brother's", "at brothers'"),
-
             ("  Tom's", "  Toms'"),
             (" book's ", " books' "),
             ("  brother's ", "  brothers' "),
@@ -104,34 +110,28 @@ class TestNouns(unittest.TestCase):
         for sing, plur in test_data:
             with self.subTest():
                 out = Noun(sing).plural()
-                self.assertEqual(out, plur,
-                                 f"Noun({sing!r}).plural()")
+                self.assertEqual(out, plur, f"Noun({sing!r}).plural()")
 
             with self.subTest():
                 out = Noun(plur).plural()
-                self.assertEqual(out, plur,
-                                 f"Noun({plur!r}).plural()")
+                self.assertEqual(out, plur, f"Noun({plur!r}).plural()")
 
             with self.subTest():
                 out = Noun(sing).singular()
-                self.assertEqual(out, sing,
-                                 f"Noun({sing!r}).singular()")
+                self.assertEqual(out, sing, f"Noun({sing!r}).singular()")
 
             with self.subTest():
                 out = Noun(plur).singular()
-                self.assertEqual(out, sing,
-                                 f"Noun({plur!r}).singular()")
+                self.assertEqual(out, sing, f"Noun({plur!r}).singular()")
 
     def test_possessive_classical(self):
         test_data = [
             ("Tom's", "Toms'"),
             ("book's", "books'"),
             ("brother's", "brethren's"),
-
             ("about Tom's", "about Toms'"),
             ("beneath book's", "beneath books'"),
             ("at brother's", "at brethren's"),
-
             ("  Tom's", "  Toms'"),
             (" book's ", " books' "),
             ("  brother's ", "  brethren's "),
@@ -139,23 +139,19 @@ class TestNouns(unittest.TestCase):
         for sing, plur in test_data:
             with self.subTest():
                 out = Noun(sing).classical().plural()
-                self.assertEqual(out, plur,
-                                 f"Noun({sing!r}).classical().plural()")
+                self.assertEqual(out, plur, f"Noun({sing!r}).classical().plural()")
 
             with self.subTest():
                 out = Noun(plur).classical().plural()
-                self.assertEqual(out, plur,
-                                 f"Noun({plur!r}).classical().plural()")
+                self.assertEqual(out, plur, f"Noun({plur!r}).classical().plural()")
 
             with self.subTest():
                 out = Noun(sing).classical().singular()
-                self.assertEqual(out, sing,
-                                 f"Noun({sing!r}).classical().singular()")
+                self.assertEqual(out, sing, f"Noun({sing!r}).classical().singular()")
 
             with self.subTest():
                 out = Noun(plur).classical().singular()
-                self.assertEqual(out, sing,
-                                 f"Noun({plur!r}).classical().singular()")
+                self.assertEqual(out, sing, f"Noun({plur!r}).classical().singular()")
 
     def test_plural_wrong_person(self):
         with self.assertRaises(ValueError):
@@ -175,6 +171,55 @@ class TestNouns(unittest.TestCase):
 
     def test_empty(self):
         self.assertEqual(Noun("").plural(), "", "Noun('').plural() => ''")
+
+    def test_is_singular_not_plural(self):
+        test_data = [
+            # "son of a guns",
+            "son of a gun",
+            "knight-errant",
+            "about book",
+            "under blanket",
+            # "book under blankets",
+            # "carpet below feet",
+        ]
+
+        # TODO:
+        # - Words known to be singular through the pattern (e.g. son of a ...) can still be
+        #   considered plural if the ... ends with s.
+        # - ".*feet" matches even "carpet under feet". This is not a problem for is_singular,
+        #   where the True is already taken, and returned. But, is_plural will take False from
+        #   (...) below (...) because the first word is singular, and *then* it reaches the
+        #   ".*feet" rule, making it True.
+
+        for sing in test_data:
+            with self.subTest():
+                self.assertTrue(
+                    Noun(sing).is_singular(), f"Noun({sing!r}).is_singular() => True"
+                )
+                self.assertFalse(
+                    Noun(sing).is_plural(), f"Noun({sing!r}).is_plural() => False"
+                )
+
+    def test_is_plural_not_singular(self):
+        test_data = [
+            "sons of gun",
+            "sons of guns",
+            "knights-errant",
+            "about books",
+            "under blankets",
+            "books under blanket",
+            "papers on table",
+        ]
+
+        for sing in test_data:
+            with self.subTest():
+                self.assertTrue(
+                    Noun(sing).is_plural(), f"Noun({sing!r}).is_plural() => True"
+                )
+                self.assertFalse(
+                    Noun(sing).is_singular(), f"Noun({sing!r}).is_singular() => False"
+                )
+
 
 if __name__ == "__main__":
     unittest.main()
